34368a40ec2844980e1801ecf76172b42e5900cb
elihuibah <elihuibh@gmail.com>
2026-04-08 16:08:09 -0700

    07 de abril de 2026

--- src/main/java/com/walmartapi/service/ProductService.java
+++ src/main/java/com/walmartapi/service/ProductService.java
@@ -1,16 +1,30 @@
 package com.walmartapi.service;
 
+import com.walmartapi.entity.ProductEntity;
 import com.walmartapi.model.Product;
+import com.walmartapi.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
-    public Product saveProduct(Product product) {
+    private final ProductRepository productRepository;
 
-        // map POJO to entity
-        // Call DB
-        // map entity to POJO
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public Product saveProduct(Product product) {
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setDescription(product.getDescription());
+        productEntity.setName(product.getName());
+        productEntity.setPrice(product.getPrice());
+
+        ProductEntity saveEntity = productRepository.save(productEntity);
+
+        product.setName(saveEntity.getName());
+        product.setDescription(saveEntity.getDescription());
+        product.setPrice(saveEntity.getPrice());
+        product.setId(saveEntity.getId());
 
         return product;
     }
